b04972cbe2298fcb840b8399eb29ab5eac91afb5
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2021-12-20 12:33:50 +0300

    Tasks sloved

---
+++ Task1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Square of number is " + (number*number));
---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int first = int.Parse(Console.ReadLine() ?? "0");
+int second = int.Parse(Console.ReadLine() ?? "0");
+if(first* first==second)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
